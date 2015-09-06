c12a809fbfe0745519231c9001ce5053bef01a87
MarinaKuziomka <marynakuziomka@ukr.net>
2015-09-06 19:36:08 +0300

    Create IdentifiersTableFull.java

---
+++ IdentifiersTableFull.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class IdentifiersTableFull {
+    public ArrayList<Identifier> parameters = new ArrayList<Identifier>();
+    public HashMap<Integer,Identifier> labels = new HashMap<Integer,Identifier>();
+    public Identifier procedureName;
+}
